3811f08fa8753e92f50d4ffcb8a66b96d11f2fc8
Nathaniel Mosco <natemosco@gmail.com>
2021-03-31 09:42:11 -0400

    moved away from deprecated function and changed response format for various endpoints

--- api/persons/personsRouter.js
+++ api/persons/personsRouter.js
@@ -11,14 +11,19 @@ router.get("/", async (req, res) => {
 
 
 router.get("/:id", async (req, res) => {
-  let input = []
-  input["term"] = req.params.id
-  input["fields"] = ["id"]
-  const singlePerson = await Controller.searchPersons(input)
+  const id = req.params.id
+  let errorMessage
+  const singlePerson = await Controller.getDetailsOfAPerson(id).catch(err => { errorMessage = err })
+  if (singlePerson && singlePerson.success) {
+    res.status(200).json(singlePerson)
+  } else {
+    res.status(errorMessage.errorCode).json(errorMessage)
+  }
 })
 
 router.post("/person", async (req, res) => {
   const { name, email, phone } = req.body
+  let errorMessage
   if (!name || !email || !phone) {
     res.status(400).json({
       "message": "requirement not met, must include valid name, email, and phone, current values posted below",
@@ -27,13 +32,17 @@ router.post("/person", async (req, res) => {
       "phone": phone,
     })
   } else {
-    const user = await Controller.addAPerson([name, email, phone])
-
-    res.status(201).json(user)
+    const user = await Controller.addAPerson([name, email, phone]).catch(err => { errorMessage = err })
+    if (user && user.success) {
+      res.status(201).json(user)
+    } else {
+      res.status(errorMessage.errorCode).json(errorMessage)
+    }
   }
 });
 router.delete("/:id", async (req, res) => {
   const id = req.params.id
+  let errorMessage
   if (!id) {
     res.status(400).json({
       "message": "id required in url params in format DELETE at endpoint: /api/persons/:id See passed input below:",
@@ -41,11 +50,19 @@ router.delete("/:id", async (req, res) => {
       "path": req.path
     })
   } else {
-    const deletedPerson = await Controller.deleteAPerson(id)
-    req.status(200).json({
-      "message": "person successfully removed",
-      "person removed": deletedPerson
-    })
+
+    const deletedPerson = await Controller.deleteAPerson(id).catch(err => { errorMessage = err })
+
+    if (deletedPerson && deletedPerson.success) {
+
+      res.status(200).json({
+        "message": "person successfully removed",
+        ...deletedPerson
+      },
+      )
+    } else {
+      res.status(errorMessage.errorCode).json({ "message": "id of user not found, unable to be deleted", ...errorMessage })
+    }
   }
 })
 
